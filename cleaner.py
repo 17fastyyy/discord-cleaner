8fd7533d1c2213f5f1999748167c48d2a3b9cb77
--global <--global>
2026-03-11 22:16:55 +0100

    added type hinting

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,4 @@
+from ty_extensions import Unknown
 import requests
 import os
 from dotenv import load_dotenv
@@ -5,19 +6,19 @@ from datetime import datetime, timedelta
 
 load_dotenv()
 
-days_since_last_msg = 3
+days_since_last_msg: int = 3
 
-md_groups = []
+md_groups: list[Unknown] = []
 
-token = os.getenv("DISCORD_TOKEN")
+token: str | None = os.getenv("DISCORD_TOKEN")
 
-headers = {"Authorization": token}
+headers: dict[str, str | None] = {"Authorization": token}
 
-response = requests.get(
+response: Unknown = requests.get(
     "https://discord.com/api/v10/users/@me/channels", headers=headers
 )
 
-res = response.json()
+res: Unknown = response.json()
 
 for channel in res:
     if channel["type"] == 3:
@@ -25,16 +26,16 @@ for channel in res:
 
 
 def snowflake_to_timestamp(snowflake_id):
-    timestamp = ((int(snowflake_id) >> 22) + 1420070400000) / 1000
+    timestamp: int | float = ((int(snowflake_id) >> 22) + 1420070400000) / 1000
     return datetime.fromtimestamp(timestamp)
 
 
 def clean_groups():
-    today = datetime.now()
+    today: datetime = datetime.now()
     count = 0
 
     for channel in md_groups[:]:
-        last_msg = snowflake_to_timestamp(channel["last_message_id"])
+        last_msg: Unknown = snowflake_to_timestamp(channel["last_message_id"])
 
         if today - last_msg > timedelta(days=1):
             print(f"Deleting group: {channel['name']}")
@@ -43,7 +44,10 @@ def clean_groups():
             )
             count += 1
 
-    print(f"You've deleted {count} groups.")
+    if count == 0:
+        print("No groups to delete matching the given requirements.")
+    else:
+        print(f"Has salido de {count} grupos")
 
 
 clean_groups()
